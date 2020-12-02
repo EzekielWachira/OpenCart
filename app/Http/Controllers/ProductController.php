5bb81e92dd28def23e1ab97f87f07749539df56d
Daniel Waiguru <danielirungu91@gmail.com>
2020-12-03 01:24:30 +0300

    fix: resolve ProductController not found error

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResourceCollection;
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index() : ProductResourceCollection {
+        return new ProductResourceCollection(Product::paginate()); 
+    }
 }
