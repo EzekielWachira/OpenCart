df47d1bd08e96af22de023d6e2466c3e0b94771c
Ezekiel Wachira <ezekielwachira048@gmail.com>
2020-12-05 00:50:52 +0300

    feat: implement login functionality

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use http\Client\Curl\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || Hash::check($request->password, $user->password)) {
+            return response([
+                'error' => 'User email or password do not match with our records'
+            ]);
+        }
+        return $user->createToken('AUTH_TOKEN')->plainTextToken;
+    }
 }
